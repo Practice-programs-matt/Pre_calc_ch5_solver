98456c1b80aca64f6efa994809c0f65bf90f84e7
mpearce25 <pearcematthew26@gmail.com>
2014-11-18 21:43:43 -0500

    question input, work on reactants

--- src/Pre_calc_ch5_solver.java
+++ src/Pre_calc_ch5_solver.java
@@ -1,4 +1,47 @@
-
-public class Pre_calc_ch5_solver {
-
+import java.util.*;
+public class Pre_calc_ch5_solver 
+{
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter your question.");
+		String question = scan.nextLine();	
+		
+		String question_short = question.replaceAll(" ", "");
+		//System.out.println(question_short);
+		
+		int question_short_length = question_short.length();
+		
+		
+		String trig_function = question_short.substring(0,3);
+		String variable = (question_short.substring(4,question_short_length-1));
+		
+		 
+		String tan = "tan";
+		String sin = "sin";
+		String cos = "cos";
+		
+		//System.out.println(trig_function);
+		System.out.println(variable);
+		
+		if (trig_function.equalsIgnoreCase(sin))
+		{
+			Sum_difference.sin(variable);
+		}
+		
+		if (trig_function.equalsIgnoreCase(cos))
+		{
+			Sum_difference.cos(variable);
+		}
+		
+		if (trig_function.equalsIgnoreCase(tan))
+		{
+			Sum_difference.tan(variable);
+		}
+		
+		System.out.println("The solution to your question is");
+	}
+	
+	
 }
---
+++ src/Sum_difference.java
@@ -0,0 +1,33 @@
+
+public class Sum_difference 
+{
+
+	public static void sin(String variable)
+	{
+		
+	}
+	
+	public static void cos(String variable)
+	{
+		
+	}
+	
+	public static void tan(String variable)
+	{
+		
+	}
+	
+	public static int reactants(int x)
+	{
+		do
+		{
+			
+			
+			
+		}
+		while (!= )
+			
+		return reactants;	
+	}
+	
+}
